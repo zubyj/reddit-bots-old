6561bc24a58cdc81b7cc834b2336da60ae1a3af6
zuby <zubyjaved@gmail.com>
2021-01-15 15:09:18 -0800

    add sleep function

--- the_office/main.py
+++ the_office/main.py
@@ -14,7 +14,7 @@ def is_logged(filename, comment_id):
             return True
     return False
 
-# Writes the reddit comment and bots response to given file.
+# Writes reddit comment and bots response to given file.
 def log_comment(filename, data, comment):
     with open(filename) as f:
         logs = json.load(f)
@@ -60,7 +60,6 @@ def get_best_match(comment, lines):
         if ratio >= highestRatio:
             bestLine = line
             highestRatio = ratio
-
     return {
         "text":bestLine["response"]["text"],
         "ratio":highestRatio,
@@ -73,11 +72,9 @@ def get_best_match(comment, lines):
 # Ex. if unique_factor is set to 5, past 5 comments must be unique.
 def is_unique_comment(filename, line_id):
     unique_factor = 5
-
     with open(filename) as f:
         data = json.load(f)
     logs = data["logs"]
-    
     length = len(logs)
     index = len(logs)
     for i in range(index, length):
@@ -104,12 +101,10 @@ def reply_comments(bot_name, lines_file, accepted_log, rejected_log):
     # Create instance of reddit account and subreddit.
     reddit = praw.Reddit(bot_name)
     subreddit = reddit.subreddit("DunderMifflin")
-
     # Open file of character's lines.
     with open(lines_file) as f:
         data = json.load(f)
     lines = data["lines"]
-
     # Check every new comment and tries to find the closest matching line
     # that the character has responded to.
     #
@@ -125,9 +120,7 @@ def reply_comments(bot_name, lines_file, accepted_log, rejected_log):
         if (counter > max_comments):
             break
         comment_len = 20
-
         bots = ["dwight-schrute-bot", "MichaelGScottBot", "andy-bernard-bot"]
-
         if (not_a_bot(comment.author, bots) and len(comment.body) >= comment_len):
             # Gets character's best matched response to the comment. 
             obj = get_best_match(comment.body, lines)
@@ -150,17 +143,22 @@ def reply_comments(bot_name, lines_file, accepted_log, rejected_log):
                 log_comment(rejected_log, obj, comment)
                 show_bot_output(comment.body, obj)
 
-# Runs the bot, logs replies, and goes to sleep.
-def run_bot(name, folder, sleep_len):
+# Replies to comments & logs replies.
+def run_bot(name, folder):
     print("RUNNING " + name)
     replies = folder + '/replies.json'
     accepted = folder + '/accepted_log.json'
     rejected = folder + '/rejected_log.json'
     reply_comments(name, replies, accepted, rejected)
+
+def sleep_time(sleep_len):
     print("SLEEPING FOR " + str(sleep_len/60) + " MINUTES")
     time.sleep(sleep_len)
 
 if __name__ == "__main__":
+    sleep_len = 300
     while (True):
-        run_bot('andy-bernard-bot', 'andy', 300)
-        run_bot('MichaelGScottBot', 'michael', 300)
+        run_bot('andy-bernard-bot', 'andy')
+        sleep_time(sleep_len)
+        run_bot('MichaelGScottBot', 'michael')
+        sleep_time(sleep_len)
