3dbfd1d838b5f5924ad2aa7faa4826fcc4d60aeb
Zuby Javed <zubyjaved@gmail.com>
2021-02-01 15:28:21 -0800

    reply to comments & log results, remove unused code

--- the_office/bot.py
+++ the_office/bot.py
@@ -1,23 +1,35 @@
+import praw
 import json
 from fuzzywuzzy import fuzz
+from datetime import datetime
+from datetime import time
 
 class bot:
     def __init__(self, name, folder):
+
+        # Logs into reddit account
         self.name = name
+        self.account = praw.Reddit(name)
+
+        # Get replies, accepted, rejected files. 
         self.folder = folder
         lines = folder + '/replies.json'
-        accepted = folder + '/accepted_log.json'
+        self.accepted_path = folder + '/accepted_log.json'
         rejected = folder + '/rejected_log.json'
-        with open(lines) as f, open(accepted) as f2, open(rejected) as f3:
+        with open(lines) as f, open(self.accepted_path) as f2, open(rejected) as f3:
             data1 = json.load(f)
             data2 = json.load(f2)
             data3 = json.load(f3)
         self.lines = data1['lines']
         self.accepted = data2['logs']
         self.rejected = data3['logs']
+        self.reply = {}
 
     def get_username(self):
         return self.name
+
+    def get_accepted_path(self):
+        return accepted_path
 
     def get_folder(self):
         return self.folder
@@ -31,8 +43,14 @@ class bot:
     def get_rejected_log(self):
         return self.rejected
 
+    def get_account(self):
+        return self.account
+    
+    def get_reply(self):
+        return self.reply
+
     # Gets character's best response to given text. 
-    def get_reply(self, text):
+    def set_reply(self, text):
         highestRatio = 0
         bestLine = self.lines[0]
         for line in self.lines:
@@ -41,7 +59,7 @@ class bot:
             if ratio > highestRatio:
                 highestRatio = ratio
                 bestLine = line
-        return {
+        self.reply =  {
             "name":self.name,
             "text":bestLine["response"]["text"],
             "ratio":highestRatio,
@@ -50,11 +68,38 @@ class bot:
             "id":bestLine["id"],
         }
 
+    def reply_comment(comment):
+        reddit = self.get_account()
+        reddit.comment(id=comment.id).reply()
+
+
     def is_logged(self, comment_id):
-        for log in get_accepted_log():
+        for log in self.get_accepted_log():
             if log['comment_id'] == comment_id:
                 return True
-        for log in get_rejected_log():
+        for log in self.get_rejected_log():
             if log['comment_id'] == comment_id:
                 return True
         return False
+
+    # Writes reddit comment and bots response to given file.
+    def log_comment(self, comment):
+        filename = self.get_folder() + '/accepted_log.json'
+        data = self.get_reply()
+        with open(filename) as f:
+            logs = json.load(f)
+        temp = logs["logs"]
+        obj = {
+            "comment":comment.body,
+            "reply":data["text"],
+            "ratio":data["ratio"],
+            "time":datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            "season":data["season"],
+            "episode":data["episode"],
+            "comment_id":comment.id,
+            "line_id":data["id"]
+        }
+        temp.append(obj)
+        with open(filename, 'w') as f:
+            json.dump(logs, f, indent=4)
+
--- the_office/main.py
+++ the_office/main.py
@@ -2,120 +2,19 @@ import praw
 import json
 from fuzzywuzzy import fuzz
 from datetime import datetime
-from datetime import time
+import time
 from bot import bot
 
-# Checks comment logs so bot doesn't reply to same comment.
-def is_logged(filename, comment_id):
-    with open(filename) as f:
-        data = json.load(f)
-    logs = data["logs"]
-    for log in logs:
-        if log["comment_id"] == comment_id:
-            return True
-    return False
-
-# Writes reddit comment and bots response to given file.
-def log_comment(filename, data, comment):
-    with open(filename) as f:
-        logs = json.load(f)
-    temp = logs["logs"]
-    obj = {
-        "comment":comment.body,
-        "reply":data["text"],
-        "ratio":data["ratio"],
-        "time":datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-        "season":data["season"],
-        "episode":data["episode"],
-        "comment_id":comment.id,
-        "line_id":data["id"]
-    }
-    temp.append(obj)
-    with open(filename, 'w') as f:
-        json.dump(logs, f, indent=4)
-
-# Function is called when bot replies to a comment.
-# Increments the reply_count of line from given id.
-def increm_reply_count(filename, id):
-    with open(filename) as f:
-        data = json.load(f)
-    lines = data["lines"]
-    for line in lines:
-        if line["id"] == id:
-            count = line["reply_count"] +1
-            line["reply_count"] = count
-            print(count)
-            print(isinstance(count, str))
-            with open(filename, 'w') as f:
-                json.dump(data, f, indent=4)
-                return
-
-# Given a reddit comment, returns the most identical line
-#   that the character has responded to.
-def get_best_match(comment, lines):
-    highestRatio = 0
-    bestLine = lines[0]
-    for line in lines:
-        text = line["line"]
-        ratio = fuzz.ratio(comment, text)
-        if ratio >= highestRatio:
-            bestLine = line
-            highestRatio = ratio
-    return {
-        "text":bestLine["response"]["text"],
-        "ratio":highestRatio,
-        "season":bestLine["season"],
-        "episode":bestLine["episode"],
-        "id":bestLine["id"]
-    }
-
-# Makes sure every few comments are unique.
-def is_unique_comment(filename, line_id):
-    unique_factor = 5
-    with open(filename) as f:
-        data = json.load(f)
-    logs = data["logs"]
-    length = len(logs)
-    index = len(logs)
-    for i in range(index, length):
-        if line_id == logs[i]["line_id"]:
-            return False
-    return True
-
-def show_bot_output(comment, obj):
-    print(comment)
-    print(obj["text"])
-    print(obj["ratio"])
-    print()
-
-def not_a_bot(author, bots):
+# Returns the bot with the best response to given text. 
+def get_bot_best_reply(text, *bots):
+    best_bot = bots[0]
+    best_bot.set_reply(text)
     for bot in bots:
-        if author == bot:
-            return False
-    return True   
-
-def get_lines(filename):
-    with open(filename) as f:
-        data = json.load(f)
-    lines = data["lines"]
-    return lines
-
-# Given all the bots responses, returns the one
-# with the best match (highest ratio)
-def get_best_ratio_res(*responses):
-    bestResponse = responses[0]
-    for response in responses:
-        if response['ratio'] > bestResponse['ratio']:
-            bestResponse = response
-    return bestResponse
-
-def get_best_response(text, *bots):
-    best_res = bots[0].get_reply(text)
-    for bot in bots:
-        res = bot.get_reply(text)
-        if res['ratio'] > best_res['ratio']:
-            best_res = res
-    return best_res
+        bot.set_reply(text)
+        ratio = bot.get_reply()['ratio']
+        if ratio > best_bot.get_reply()['ratio']:
+            best_bot = bot
+    return best_bot
 
 # Comment is valid if 
 #   1. Longer than min_length
@@ -138,64 +37,28 @@ def is_valid_comment(comment, bots):
                 return False
     return True
 
-def run_the_bot(*bots):
-    reddit = praw.Reddit('dwight-schrute-bot')
-    accepted_ratio = 60
+def run_the_bots(*bots):
+    reddit = bots[0].get_account()
+    accepted_ratio = 70
     for submission in reddit.subreddit('all').rising(limit=10):
         submission.comments.replace_more(limit=None)
         for comment in submission.comments.list():
             if is_valid_comment(comment, bots):
-                res = get_best_response(comment.body, dwight, michael)
-                ratio = res['ratio']
-                if ratio > accepted_ratio:
-                    print(ratio)
-                    print(res['name'])
-                    print("COMMENT " + comment.body)
-                    print("RESPONSE " + res['text'])
-                    print()
-
-def reply_comments(bot_name, lines_file, accepted_log, rejected_log):
-    # If ratio meets set minimum, log comment & reply in accepted log.
-    reddit = praw.Reddit(bot_name)
-    lines = get_lines(lines_file)
-    min_ratio = 50
-    min_rej_ratio = 48
-    for submission in reddit.subreddit("all").rising(limit=10):
-        # Makes sure the last comment isn't a MoreComments object. 
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list():
-            bots = ["dwight-schrute-bot", "MichaelGScottBot", "andy-bernard-bot"]
-            min_comment_len = 20
-            if (not_a_bot(comment.author, bots) and len(comment.body) >= min_comment_len):
-                obj = get_best_match(comment.body, lines)
-                ratio = obj["ratio"]
-                if ratio > 45:
-                    print("COMMENT : " + comment.body + " RATIO : " + str(ratio))
-                    print("REPLY " + obj["text"])
-                    print()
-                if ratio >= min_ratio:
-                    log = accepted_log
-                    if not is_logged(log, comment.id) and is_unique_comment(log, obj["id"]):
-                        print("ACCEPTED")
-                        comment.reply(obj["text"])
-                        log_comment(log, obj, comment)
-                        increm_reply_count(lines_file, obj["id"])
-                        show_bot_output(comment.body, obj)
-                        time.sleep(180)
-
-                # If ratio meets rejected minimum, log comment & reply in rejected.
-                elif ratio >= min_rej_ratio and not is_logged(rejected_log, comment.id):
-                    print("REJECTED")
-                    log_comment(rejected_log, obj, comment)
-                    show_bot_output(comment.body, obj)
-
-# Replies to comments & logs replies.
-def run_bot(username, folder):
-    print("RUNNING " + username)
-    replies = folder + '/replies.json'
-    accepted = folder + '/accepted_log.json'
-    rejected = folder + '/rejected_log.json'
-    reply_comments(username, replies, accepted, rejected)
+                # res = get_best_response(comment.body, dwight, michael)
+                bot = get_bot_best_reply(comment.body, dwight, michael)
+                ratio = bot.get_reply()['ratio']
+                if not bot.is_logged(comment.id):
+                    if ratio > accepted_ratio:
+                            # the_comment is attached to bot that is going to reply. 
+                            comment = bot.get_account().comment(id=comment.id)
+                            comment.reply(bot.get_reply()['text'])
+                            bot.log_comment(comment)
+                            # bot.get_account().comment(id=comment.id).reply()
+                            print(ratio)
+                            print("COMMENT " + comment.body)
+                            print(bot.get_reply()['text'])
+                            print()
+                            time.sleep(60)
 
 def sleep_time(sleep_len):
     print("SLEEPING FOR " + str(sleep_len/60) + " MINUTES") 
@@ -206,7 +69,37 @@ if __name__ == "__main__":
         #run_bot('MichaelGScottBot', 'michael')
         #run_bot('dwight-schrute-bot', 'dwight')
         dwight = bot('dwight-schrute-bot', 'dwight')
-        michael = bot('michaelGScottBot', 'michael')
-        run_the_bot(dwight, michael)
-        sleep_time(180)
+        michael = bot('MichaelGScottBot', 'michael')
+        run_the_bots(dwight, michael)
         # run_bot('andy-bernard-bot', 'andy')
+
+
+# Function is called when bot replies to a comment.
+# Increments the reply_count of line from given id.
+# def increm_reply_count(filename, id):
+#     with open(filename) as f:
+#         data = json.load(f)
+#     lines = data["lines"]
+#     for line in lines:
+#         if line["id"] == id:
+#             count = line["reply_count"] +1
+#             line["reply_count"] = count
+#             print(count)
+#             print(isinstance(count, str))
+#             with open(filename, 'w') as f:
+#                 json.dump(data, f, indent=4)
+#                 return
+
+
+# Makes sure every few comments are unique.
+# def is_unique_comment(filename, line_id):
+#     unique_factor = 5
+#     with open(filename) as f:
+#         data = json.load(f)
+#     logs = data["logs"]
+#     length = len(logs)
+#     index = len(logs)
+#     for i in range(index, length):
+#         if line_id == logs[i]["line_id"]:
+#             return False
+#     return True
