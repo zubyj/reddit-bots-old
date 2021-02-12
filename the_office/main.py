da9e7d0dbda0f8fa63f6251f2fef58224098bbad
root <zubyjaved@gmail.com>
2021-02-11 19:01:47 -0800

    update logs and ignore 403 errors

--- the_office/main.py
+++ the_office/main.py
@@ -4,6 +4,8 @@ from fuzzywuzzy import fuzz
 from datetime import datetime
 import time
 from bot import bot
+from prawcore.exceptions import Forbidden
+
 
 def get_bot_best_reply(text, *bots):
     # Returns the bot with best reply to given text.
@@ -24,7 +26,7 @@ def is_valid_comment(comment, bots):
     #   1. Our bots didnt write it.
     #   2. Our bots haven't yet replied to it.
     #   3. It's longer than min_length
-    min_len = 20
+    min_len = 15
     if len(comment.body) < min_len:
         return False
     for bot in bots:
@@ -49,7 +51,7 @@ def run_the_bots(*bots):
     #   3. Delete any past 5 replies with negative karma.
     #   4. Sleep for 3 minutes
     reddit = bots[0].get_account()
-    accepted_ratio = 70
+    accepted_ratio = 75
     for submission in reddit.subreddit('all').rising(limit=15):
         submission.comments.replace_more(limit=None)
         if not submission.over_18:
@@ -59,26 +61,26 @@ def run_the_bots(*bots):
                     ratio = bot.get_reply()['ratio']
                     if not bot.is_logged(comment.id):
                         if ratio > accepted_ratio:
-                                comment = bot.get_account().comment(id=comment.id)
-                                reply = bot.get_reply()
+                            comment = bot.get_account().comment(id=comment.id)
+                            reply = bot.get_reply()
+                            try:
                                 comment.reply(reply['text'])
-                                bot.log_comment(comment)
-                                bot.del_neg_comments()
-                                print(ratio)
-                                print("COMMENT " + comment.body)
-                                print(bot_reply['text'])
-                                print()
-                                sleep_time(180)
+                            except Forbidden:
+                                print(f"We\'ve been banned on r/{comment.subreddit}!")
+                            bot.log_comment(comment)
+                            bot.del_neg_comments()
+                            sleep_time(120)
 
 def sleep_time(sleep_len):
     print("SLEEPING FOR " + str(sleep_len/60) + " MINUTES") 
+    print()
     time.sleep(sleep_len)
 
 if __name__ == "__main__":
     while (True):
         dwight = bot('dwight-schrute-bot', 'dwight')
         michael = bot('MichaelGScottBot', 'michael')
-        dwight.del_neg_comments()
-        michael.del_neg_comments()
-        print('RUNNING THE BOTS')
+        print('STARTING THE BOTS AGAIN')
         run_the_bots(dwight, michael)
+        print('DONE RUNNING')
+        sleep_time(180)
