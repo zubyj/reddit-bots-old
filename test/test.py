9b5dc033868226605e3ee112fb7668e1a3047ceb
root <zubyjaved@gmail.com>
2021-08-15 14:12:45 -0700

    Update logs

--- test/test.py
+++ test/test.py
@@ -3,14 +3,9 @@ import praw
 from main import get_best_match
 
 reddit = praw.Reddit("dwight-schrute-bot")
-subreddit = reddit.subreddit("DunderMifflin")
 
-with open("dwight/replies.json") as f:
-    data = json.load(f)
-lines = data["lines"]
-
-for line in lines:
-    del line["accepted_ratio"]
-
-with open('test.json', 'w') as f:
-    json.dump(data, f, indent=4)
+for submission in reddit.subreddit('all').rising(limit=15):
+    submission.comments.replace_more(limit=None)
+    if not submission.over_18:
+        for comment in submission.comments.list():
+            print(comment)
